02bb654f4abf6c5b3d36a01c82bbea818b406fec
Sebastian Remaggi <ext_sebasremaggi@falabella.cl>
2022-06-26 19:48:25 -0400

    agrego iicons

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { ImageBackground,Image,StyleSheet, Text, View,Button,Alert } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {SocialIcon,Icon,Fo} from "react-native-elements";
+import { ImageBackground,Image,StyleSheet, Text, View,Button,Alert,TouchableOpacity } from "react-native";
 
 const Separator = () => (
   <View style={styles.separator} />
@@ -9,30 +12,58 @@ const Separator = () => (
 const App = () => (
   <View style={styles.container}>
     <ImageBackground source={require('./casa.jpg')} resizeMode="cover" style={styles.imageBackground}>
+     
       <View style={styles.topContainer} >
-
+      <SocialIcon  iconColor="#00BD33" style={styles.login} button type="user" title="Login" onPress={()=> Alert.alert("Login")}/>
       </View>
-
 
       <View style={styles.middleContainer}>
       <Text style={styles.text}>Hospedaje Primera Vista</Text>
       </View>
-      
-      <Separator/>
-      <View style={styles.bottonContainer}>
-      <Image 
-       source={{uri:'https://picsum.photos/100'}}
-       style={styles.image}
-      />
-       <Image 
-       source={{uri:'https://picsum.photos/200'}}
-       style={styles.image}
-      />
-       <Image 
-       source={{uri:'https://picsum.photos/300'}}
-       style={styles.image}
-      />
+
+      <View style={styles.lineImages}>
+
+      <TouchableOpacity style={styles.button}>
+      <FontAwesomeIcon icon="0" />
+      <Text  style={{textAlign:'center'}}>CARCTERÍSTICAS</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+      <SocialIcon  button type="address-card"/>
+      <Text  style={{textAlign:'center'}}>DISPONIBILIDAD</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+      <SocialIcon  button type="camera"/>
+      <Text  style={{textAlign:'center'}}>UBICACIÓN</Text>
+      </TouchableOpacity>
+     
       </View>
+      <View style={styles.lineImages}>
+
+      <TouchableOpacity style={styles.button}>
+      <SocialIcon  button type="camera"/>
+      <Text  style={{textAlign:'center'}}>TÚ RESERVA</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+      <SocialIcon  button type="camera"/>
+      <Text  style={{textAlign:'center'}}>EXPERIENCIAS</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+      <SocialIcon  button type="address-card"/>
+      <Text  style={{textAlign:'center'}}>CONTACTO</Text>
+      </TouchableOpacity>
+     
+      </View>
+
+      
+      <View style={styles.bottonContainer}>
+
+      </View>
+  
+      
   
     </ImageBackground>
   </View>
@@ -44,17 +75,34 @@ const styles = StyleSheet.create({
     
   },
   button:{
-    color:'green',
-  },
-  topContainer: {
-    flex: 5,
+  width:'30%',
+  height:'90%',
+  borderWidth:0.3,
+  color:'white',
+  borderColor:'white',
+  borderRadius:10,
+  padding:10,
 
-    justifyContent:'center',
-    alignItems:'center',
+},
+  topContainer: {
+    flex: 1,
+    flexDirection:'column-reverse',
+    alignItems:'flex-end',
+    direction:'RTL',
+
   },
   middleContainer: {
-    justifyContent:'flex-start',  
-    flex:1,
+   marginTop:20,
+   marginBottom:20,
+    justifyContent:'center',
+    textAlign:'center',  
+    flex:0.3,
+  },
+
+  lineImages:{
+    flex:0.8,
+    flexDirection:'row',
+    justifyContent:'space-around',
   },
   buttonsContainer: {
     flex: 1,
@@ -66,24 +114,25 @@ const styles = StyleSheet.create({
 
   bottonContainer:{
     marginTop:20,
-    justifyContent:'space-around',
     flexDirection:'row',
     padding:10,
-    flex:10,
+    flex:2,
   },
 
-  image: {height:150,width:280,borderColor:'white',borderRadius:10},
+  image: {height:'80%',width:'25%',
+  borderColor:'white',borderRadius:10,borderWidth:0.3,color:'white'},
+  login: {height:'40%',width:'20%',borderColor:'white',borderRadius:10, fontSize:10,},
   imageBackground: {
     flex: 1,
     justifyContent: "center"
   },
   text: {
-    color: "white",
-    fontSize: 48,
-    lineHeight: 50,
-    textAlign: "center",
-    backgroundColor: "#01BB33",
-    opacity:0.8
+    opacity:0.8,
+    color:'white',
+    backgroundColor:'#00BD33',
+    lineHeight:50,
+    fontSize:30,
+    textAlign:'center'
   },
   separator: {
     marginVertical: 10,
