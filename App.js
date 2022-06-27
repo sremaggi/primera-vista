b8cc7bd5f1752d19e8e4ce6611c8d52e012f3e78
Sebastian Remaggi <ext_sebasremaggi@falabella.cl>
2022-06-27 02:08:31 -0400

    cambios login

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import {SocialIcon,Icon,Fo} from "react-native-elements";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import {SocialIcon} from "react-native-elements";
 import { ImageBackground,Image,StyleSheet, Text, View,Button,Alert,TouchableOpacity } from "react-native";
+import { color } from "react-native-elements/dist/helpers";
 
 const Separator = () => (
   <View style={styles.separator} />
 );
+
+const ButtonName = (props,navigation) => (
+  <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('CaracteristicasPage')}>
+  <Ionicons style={styles.homeIcon} name={props.name} size={40} color='white'  />
+  <Text  style={{textAlign:'center', color:'white',fontWeight:'bold',fontSize:9}}>{props.text}</Text>
+  </TouchableOpacity>
+);
+
+const Login = () => (
+  <TouchableOpacity style={styles.loginButton}>
+  <Ionicons style={styles.homeIcon} name="md-arrow-up-circle-sharp" size={32} color='black'  />
+  <Text  style={{textAlign:'center', color:'black',fontWeight:'bold',fontSize:8}}>INGRESAR</Text>
+  </TouchableOpacity>
+);
+
+const Register = () => (
+  <TouchableOpacity style={styles.loginButton}>
+  <Ionicons style={styles.homeIcon} name="md-arrow-down-circle-sharp" size={32} color='black'  />
+  <Text  style={{textAlign:'center', color:'black',fontWeight:'bold',fontSize:8}}>REGISTRAR</Text>
+  </TouchableOpacity>
+);
+
+const DisponibilidadVista = ()=>{
+
+};
+
+class CaracteristicasPage extends React.Component {  
+  render() {  
+      return (  
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>  
+              <Text>Profile Screen</Text>  
+          </View>  
+  );  
+  }  
+}  
 
 
 const App = () => (
@@ -14,7 +51,9 @@ const App = () => (
     <ImageBackground source={require('./casa.jpg')} resizeMode="cover" style={styles.imageBackground}>
      
       <View style={styles.topContainer} >
-      <SocialIcon  iconColor="#00BD33" style={styles.login} button type="user" title="Login" onPress={()=> Alert.alert("Login")}/>
+      <Button title="Go to Home" onPress={() => navigation.navigate(CaracteristicasPage)} />
+      <Login></Login>
+      <Register></Register>
       </View>
 
       <View style={styles.middleContainer}>
@@ -23,38 +62,16 @@ const App = () => (
 
       <View style={styles.lineImages}>
 
-      <TouchableOpacity style={styles.button}>
-      <FontAwesomeIcon icon="0" />
-      <Text  style={{textAlign:'center'}}>CARCTERÍSTICAS</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-      <SocialIcon  button type="address-card"/>
-      <Text  style={{textAlign:'center'}}>DISPONIBILIDAD</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-      <SocialIcon  button type="camera"/>
-      <Text  style={{textAlign:'center'}}>UBICACIÓN</Text>
-      </TouchableOpacity>
+      <ButtonName name="home" text='CARACTERÍSTICAS'></ButtonName>
+      <ButtonName name="calendar" text='DISPONIBILIDAD'></ButtonName>
+      <ButtonName name="location" text='UBICACIÓN'></ButtonName>
      
       </View>
       <View style={styles.lineImages}>
 
-      <TouchableOpacity style={styles.button}>
-      <SocialIcon  button type="camera"/>
-      <Text  style={{textAlign:'center'}}>TÚ RESERVA</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-      <SocialIcon  button type="camera"/>
-      <Text  style={{textAlign:'center'}}>EXPERIENCIAS</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-      <SocialIcon  button type="address-card"/>
-      <Text  style={{textAlign:'center'}}>CONTACTO</Text>
-      </TouchableOpacity>
+      <ButtonName name="book" text='RESERVA'></ButtonName>
+      <ButtonName name="star" text='EXPERIENCIAS'></ButtonName>
+      <ButtonName name="call" text='CONTACTO'></ButtonName>
      
       </View>
 
@@ -70,33 +87,47 @@ const App = () => (
 );
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     
   },
+  loginButton:{
+    borderWidth:0.3,
+    borderColor:'white',
+    borderRadius:20,
+    height:'40%',
+    width:'20%',
+    margin:5
+  },
   button:{
-  width:'30%',
-  height:'90%',
-  borderWidth:0.3,
-  color:'white',
-  borderColor:'white',
-  borderRadius:10,
-  padding:10,
+    width:'30%',
+    height:'90%',
+    borderWidth:0.3,
+    flexDirection:'column',
+    color:'white',
+    justifyContent:'center',
+    borderColor:'white',
+    borderRadius:10,
+   padding:10,
 
 },
   topContainer: {
     flex: 1,
-    flexDirection:'column-reverse',
+  
+    flexDirection:'row-reverse',
     alignItems:'flex-end',
     direction:'RTL',
+    padding:10,
 
   },
   middleContainer: {
-   marginTop:20,
+    opacity:0.9,
+    backgroundColor:'#00BD33',
    marginBottom:20,
-    justifyContent:'center',
-    textAlign:'center',  
-    flex:0.3,
+  justifyContent:'center',
+  textAlign:'center',  
+    flex:0.6,
   },
 
   lineImages:{
@@ -104,13 +135,6 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     justifyContent:'space-around',
   },
-  buttonsContainer: {
-    flex: 1,
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center',
-  },
- 
 
   bottonContainer:{
     marginTop:20,
@@ -119,18 +143,38 @@ const styles = StyleSheet.create({
     flex:2,
   },
 
-  image: {height:'80%',width:'25%',
-  borderColor:'white',borderRadius:10,borderWidth:0.3,color:'white'},
-  login: {height:'40%',width:'20%',borderColor:'white',borderRadius:10, fontSize:10,},
+  image: {
+    height:'80%',
+    width:'25%',
+    borderColor:'white',
+    borderRadius:10,
+    borderWidth:0.3,
+    color:'white'
+  },
+
+  login: {
+    height:'40%',
+    width:'20%',
+    borderColor:'white',
+    borderRadius:10,
+    fontSize:10
+  },
   imageBackground: {
     flex: 1,
     justifyContent: "center"
   },
+  homeIcon:{
+    textAlign:'center',
+    margin:2
+    
+  },
   text: {
-    opacity:0.8,
+  fontFamily:"Georgia",
+    fontWeight:'bold',
+    opacity:1,
+    justifyContent:'center',
+
     color:'white',
-    backgroundColor:'#00BD33',
-    lineHeight:50,
     fontSize:30,
     textAlign:'center'
   },
